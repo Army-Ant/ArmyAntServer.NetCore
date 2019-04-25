ccc55298cc3a7b278138420e73229d7ee0c564e7
Jason Zhao Jie <261343578@qq.com>
2019-04-25 20:18:02 +0800

    Add protobuffer ref

---
+++ src/Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArmyAnt.Server {
+    static class Program {
+        public static int Main(params string[] arg) {
+
+            return 0;
+        }
+    }
+}
