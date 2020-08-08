ff50dfbebab9fd080f1761268eae1bd72d25f566
Jason-Zhao-Jie <261343578@qq.com>
2020-08-08 11:45:05 +0800

    update submodule

--- external/build_protobuf.bat
+++ external/build_protobuf.bat
@@ -1,6 +1,14 @@
-cd protobuf
-git checkout master
 git submodule update --init --progress --recursive
+cd ArmyAnt.js
+git checkout master
+cd ../ArmyAnt.Net
+git checkout master
+cd ../MySqlConnector
+git checkout master
+cd ../../res
+git checkout master
+cd ../external/protobuf
+git checkout master
 cd cmake
 mkdir build_win
 cd build_win
@@ -14,3 +22,4 @@ cd ../release
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../.. ../..
 nmake
 nmake install
+pause
